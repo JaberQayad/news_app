1e015b5d255587658e902aef769326637e79ec14
JaberQayad <jaberqayad@gmail.com>
2021-12-26 20:33:06 +0100

    add navMenu

--- lib/shared_ui/navigation_drawer.dart
+++ lib/shared_ui/navigation_drawer.dart
@@ -8,18 +8,47 @@ class NavigationDrawer extends StatefulWidget {
 }
 
 class _NavigationDrawerState extends State<NavigationDrawer> {
+
+  List <String> navMenu = [
+
+    'Explore',
+    'HeadLine News',
+    'Videos',
+    'Photos',
+    'Settings',
+    'Logout'
+  ];
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView.builder(
-        itemBuilder: (context, position) {
-          return ListTile(
-            title: Text('HOME', style: TextStyle(color: Colors.grey.shade700),),
-            trailing: Icon(Icons.chevron_right, color: Colors.grey.shade400,),
-            onTap: (){},
-          );
-        },
-        itemCount: 8,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 75, left: 24),
+        child: ListView.builder(
+          itemBuilder: (context, position) {
+            return Padding(
+              padding: const EdgeInsets.all(8),
+              child: ListTile(
+                title: Text(
+                  navMenu[position],
+                  style: TextStyle(
+                    color: Colors.grey.shade700,
+                    fontSize: 22,
+                  ),
+                ),
+                trailing: Icon(
+                  Icons.chevron_right,
+                  color: Colors.grey.shade400,
+                ),
+                onTap: () {},
+              ),
+            );
+          },
+          itemCount: navMenu.length,
+        ),
       ),
     );
   }
